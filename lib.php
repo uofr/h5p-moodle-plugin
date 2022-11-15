55357fa889b6fcb639c68d4dd14ddc50f85b00db
Trevor Cunningham <trevor.cunningham@uregina.ca>
2022-11-15 15:05:04 -0600

    adds purpose support for Moodle 4

--- lib.php
+++ lib.php
@@ -68,7 +68,8 @@ function hvp_supports($feature) {
             return true;
         case FEATURE_SHOW_DESCRIPTION:
             return true;
-
+        case FEATURE_MOD_PURPOSE:
+            return MOD_PURPOSE_CONTENT;
         default:
             return null;
     }
